7ce8b3d2c055b7b9d79f2b25a6971e2b210a1be0
GGTuran <tayyabturan67@gmail.com>
2024-04-07 16:35:56 +0600

    loading sinfle service data by get operation

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
@@ -14,7 +14,54 @@ console.log(process.env.DB_USER)
 
 
 
+// 4.1
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vddbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+
+//     const serviceCollection = client.db('carDoctor').collection('services');
+
+
+//     app.get('/services', async(req,res) =>{
+//       const cursor = serviceCollection.find();
+//       const result = await cursor.toArray();
+//       res.send(result);
+//     })
+
+
+
+
+
+
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+// 5.5
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vddbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +79,7 @@ async function run() {
     await client.connect();
 
 
+    
     const serviceCollection = client.db('carDoctor').collection('services');
 
 
@@ -40,6 +88,22 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    app.get('/services/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+
+      const options = {
+       
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {  title: 1, price: 1, service_id:1 },
+      };
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    })
+
+
 
 
 
@@ -55,6 +119,7 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
 
 
 
